9ac675de39a9574765c06c93dcbf3ae7e2133d40
nicky <Schroedernick2@aol.com>
2020-01-13 16:08:49 -0500

    Added Foreground definitions

--- color.h
+++ color.h
@@ -1,23 +1,30 @@
 #ifndef COLOR_H
 #define COLOR_H
 
-#define BLACK(X)	"\033[9;30m" X "\033[0m"
-#define RED(X)		"\033[1;31m" X "\033[0m"
-#define GREEN(X)	"\033[1;32m" X "\033[0m"
-#define YELLOW(X)	"\033[1;33m" X "\033[0m"
-#define BLUE(X)		"\033[1;34m" X "\033[0m"
-#define MAGENTA(X)	"\033[1;35m" X "\033[0m"
-#define CYAN(X)		"\033[1;36m" X "\033[0m"
-#define WHITE(X)	"\033[1;37m" X "\033[0m"
+#define DEFAULT     0
+#define BOLD        1
+#define DIM         2
+#define ITALIC      3
+#define UNDERLINED  4
+#define REVERSE     7
+#define HIDDEN      8
+#define CROSSED     9
 
-#define DEFAULT		0
-#define BOLD		1
-#define DIM			2
-#define ITALIC		3
-#define UNDERLINED	4
-#define REVERSE		7
-#define HIDDEN		8
-#define CROSSED		9
+#define BLACK_FG        30
+#define RED_FG          31
+#define GREEN_FG        32
+#define YELLOW_FG       33
+#define BLUE_FG         34
+#define MAGENTA_FG      35
+#define CYAN_FG         36
+#define WHITE_FG        37
+#define BBLACK_FG       90
+#define BRED_FG         91
+#define BGREEN_FG       92
+#define BYELLOW_FG      93
+#define BBLUE_FG        94
+#define BMAGENTA_FG     95
+#define BCYAN_FG        96
 
 namespace COLOR_HANDLER {
 
@@ -27,7 +34,8 @@ namespace COLOR_HANDLER {
 			~color_manager();
 
 		private:
-			int attribute;
+			int font_attribute;
+			
 	};
 
 }
