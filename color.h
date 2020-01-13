6e7cea31ca0b632436cc283116f6fca60f15c251
nicky <Schroedernick2@aol.com>
2020-01-13 16:11:03 -0500

    Added Background definitions

--- color.h
+++ color.h
@@ -26,6 +26,23 @@
 #define BMAGENTA_FG     95
 #define BCYAN_FG        96
 
+#define BLACK_BG        40
+#define RED_BG          41
+#define GREEN_BG        42
+#define YELLOW_BG       43
+#define BLUE_BG         44
+#define MAGENTA_BG      45
+#define CYAN_BG         46
+#define WHITE_BG        47
+#define BBLACK_BG       100
+#define BRED_BG         101
+#define BGREEN_BG       102
+#define BYELLOW_BG      103
+#define BBLUE_BG        104
+#define BMAGENTA_BG     105
+#define BCYAN_BG        106
+
+
 namespace COLOR_HANDLER {
 
 	class color_manager {
